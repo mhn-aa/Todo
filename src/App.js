020fe8a33ebdeca367d7722f3b0aaa72cecb6e4a
mhn-aa <mahanmirjamali@gmail.com>
2021-09-08 14:56:32 +1000

    id-math

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
 
   useEffect(() => {
-    const storedTodos =  JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) setTodos(storedTodos)
-  },[])
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  },[todos] )
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  console.log(todos);
 
   function toggleTodo(id) {
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id ===id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
   }
 
   function handleAddTodo(e) {
-    const name = todoNameRef.current.value
-    if (name === '') return
-    setTodos (prevTodos => {return [...prevTodos, { id: 1, name: name, complete:false}]})
-    todoNameRef.current.value = null
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: Math.random(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null;
   }
   return (
     <>
@@ -37,7 +41,7 @@ function App() {
       <button>Clear Completed</button>
       <div>0 left to do</div>
     </>
-  )
+  );
 }
 
 export default App;
