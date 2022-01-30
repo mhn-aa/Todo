95297d08829fda7c5ff8fe1dda39ace4ed3cec3c
mhn-aa <mahanmirjamali@gmail.com>
2022-01-30 16:46:03 +1100

    NH30Jan

--- src/App.js
+++ src/App.js
@@ -48,30 +48,29 @@ function App() {
         <div>X2</div>
         <div>X3</div>
       </div>
-      <div className="MegaTODO">
-        <div className="todolist">
-          <TodoList todos={todos} toggleTodo={toggleTodo} />
-        </div>
-        <div className="todos">
-          <input className="the_box" ref={todoNameRef} type="text" />
-        </div>
-        <div className="addtodo">
-          <button onClick={handleAddTodo}>Add Todo</button>
-        </div>
-        <div className="clear">
-          <button onClick={handleClearTodos}>Clear Completed</button>
-        </div>
-        <div className="completed">
-          {todos.filter((todo) => !todo.complete).length} left to do
-        </div>
-        <div className="Para">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
-            blanditiis quam aperiam officia nostrum unde. Veritatis unde tempora
-            optio illo ipsa voluptatum, maxime vitae ipsum eaque facilis odit
-            autem officia.
-          </p>
-        </div>
+
+      <div className="todolist">
+        <TodoList todos={todos} toggleTodo={toggleTodo} />
+      </div>
+      <div className="todos">
+        <input className="the_box" ref={todoNameRef} type="text" />
+      </div>
+      <div className="addtodo">
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </div>
+      <div className="clear">
+        <button onClick={handleClearTodos}>Clear Completed</button>
+      </div>
+      <div className="completed">
+        {todos.filter((todo) => !todo.complete).length} left to do
+      </div>
+      <div className="Para">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus
+          blanditiis quam aperiam officia nostrum unde. Veritatis unde tempora
+          optio illo ipsa voluptatum, maxime vitae ipsum eaque facilis odit
+          autem officia.
+        </p>
       </div>
 
       <div className="Footer">
